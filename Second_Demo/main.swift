e7eb3bde86ee9f65a0c4697142e423002432ff51
Bob Peng <geekthinking@gmail.com>
2015-03-10 08:33:26 -0700

    Initial Commit

---
+++ Second_Demo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Second_Demo
+//
+//  Created by Local Admin on 2015-03-10.
+//  Copyright (c) 2015 Local Admin. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
